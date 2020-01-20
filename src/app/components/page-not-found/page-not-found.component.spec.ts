d5b17614d677cebccd3bedc6ddb2cbef544939b8
John Roxborough <johnmrox@gmail.com>
2020-01-20 04:08:19 -0600

    add h3 tests

--- src/app/components/page-not-found/page-not-found.component.spec.ts
+++ src/app/components/page-not-found/page-not-found.component.spec.ts
@@ -1,25 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PageNotFoundComponent } from './page-not-found.component';
 
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
   let fixture: ComponentFixture<PageNotFoundComponent>;
+  let h3: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ PageNotFoundComponent ]
-    })
-    .compileComponents();
+    });
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PageNotFoundComponent);
     component = fixture.componentInstance;
+    h3 = fixture.nativeElement.querySelector('h3');
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the PageNotFoundComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the page not found message', () => {
+    expect(h3).toBeDefined();
+    expect(h3.textContent).toContain('This page was not found');
   });
 });
