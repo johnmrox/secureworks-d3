282cb2f24e493cb95d9f65adc85eb523d98910f4
John Roxborough <johnmrox@gmail.com>
2020-01-19 23:59:39 -0600

    remove detectChanges in each unit test as that is provided in the beforeEach function

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
 import {MatToolbarModule} from '@angular/material';
 import {RouterTestingModule} from '@angular/router/testing';
@@ -49,37 +48,31 @@ describe('HeaderComponent', () => {
   });
 
   it('should contain the toolbar element', () => {
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('mat-toolbar')).toBeDefined();
   });
 
   it('should contain a nav element', () => {
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('nav')).toBeDefined();
   });
 
   it('should contain two buttons', () => {
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelectorAll('button').length).toBe(2);
   });
 
   it('should render the Add Client button', () => {
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelectorAll('button')[0].textContent).toContain('Add Client');
   });
 
   it('should render the Analytics button', () => {
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelectorAll('button')[1].textContent).toContain('Analytics');
   });
 
   it('should render the logo div', () => {
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('.logo')).toBeDefined();
     expect(compiled.querySelector('.logo').textContent).toContain('Client App');
